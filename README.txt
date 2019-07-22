b1cae6445e9f7cebbc7d2bee53ac7ef7ca4f5d47
Yury Demidenko <demidenko05@gmail.com>
2019-07-22 16:50:54 +0300

    calendar test

--- README.txt
+++ README.txt
@@ -6,6 +6,8 @@ It's based on beige-bcommon.
 It consists of common business logic, e.g. persistable models to store languages, countries, users,
 abstraction and implementations of services like ORM, JDBC, CSV writer, etc.
 
+To compile you need PostgreSql and MySql databases bsblct with user/password beigeaccounting, see test package for details.
+
 debug range #5-8 (5000..8999)
 
 licenses:
--- src/test/java/org/beigesoft/test/CalendarTest.java
+++ src/test/java/org/beigesoft/test/CalendarTest.java
@@ -32,6 +32,7 @@ import java.util.Date;
 import java.util.Calendar;
 import java.util.Locale;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
@@ -51,33 +52,33 @@ public class CalendarTest {
     Date date = new Date(1470225720000L); // 3 Aug 2016 15:02
     System.out.println("t1 - date is " + date);
 
-    Date periodDayStart = evalDatePeriodStartFor(date, EPeriod.DAILY);
+    Date periodDayStart = evDatePeriodStart(date, EPeriod.DAILY, this.cal);
     System.out.println("t1 - period day start is " + periodDayStart);
     assertEquals(new Date(1470171600000L), periodDayStart); //Wed Aug 03 2016 00:00:00
-    Date periodWeekStart = evalDatePeriodStartFor(date, EPeriod.WEEKLY);
+    Date periodWeekStart = evDatePeriodStart(date, EPeriod.WEEKLY, this.cal);
     System.out.println("t1 - period week start is " + periodWeekStart);
     assertEquals(new Date(1469912400000L), periodWeekStart); //Sun Jul 31 2016 00:00:00 USA standard!!!
-    Date periodMonthStart = evalDatePeriodStartFor(date, EPeriod.MONTHLY);
+    Date periodMonthStart = evDatePeriodStart(date, EPeriod.MONTHLY, this.cal);
     System.out.println("t1 - period month start is " + periodMonthStart);
     assertEquals(new Date(1469998800000L), periodMonthStart); //Mon Aug 01 2016 00:00:00
 
-    Date nextDayStart = evalDateNextPeriodStart(date, EPeriod.DAILY);
+    Date nextDayStart = evalDateNextPeriodStart(date, EPeriod.DAILY, this.cal);
     System.out.println("t1 - next day start is " + nextDayStart);
     assertEquals(new Date(1470258000000L), nextDayStart); //Thu Aug 04 2016 00:00:00
-    Date nextWeekStart = evalDateNextPeriodStart(date, EPeriod.WEEKLY);
+    Date nextWeekStart = evalDateNextPeriodStart(date, EPeriod.WEEKLY, this.cal);
     System.out.println("t1 - next week start is " + nextWeekStart);
     assertEquals(new Date(1470517200000L), nextWeekStart); //Sun Jul 07 2016 00:00:00 USA standard!!!
-    Date nextMonthStart = evalDateNextPeriodStart(date, EPeriod.MONTHLY);
+    Date nextMonthStart = evalDateNextPeriodStart(date, EPeriod.MONTHLY, this.cal);
     System.out.println("t1 - next month start is " + nextMonthStart);
     assertEquals(new Date(1472677200000L), nextMonthStart); //Thu Sep 01 2016 00:00:00
 
-    Date prevDayStart = evalDatePrevPeriodStart(date, EPeriod.DAILY);
+    Date prevDayStart = evDatePrevPeriodStart(date, EPeriod.DAILY, this.cal);
     System.out.println("t1 - prev day start is " + prevDayStart);
     assertEquals(new Date(1470085200000L), prevDayStart); //Thu Aug 02 2016 00:00:00
-    Date prevWeekStart = evalDatePrevPeriodStart(date, EPeriod.WEEKLY);
+    Date prevWeekStart = evDatePrevPeriodStart(date, EPeriod.WEEKLY, this.cal);
     System.out.println("t1 - prev week start is " + prevWeekStart);
     assertEquals(new Date(1469307600000L), prevWeekStart); //Sun Jul 24 2016 00:00:00 USA standard!!!
-    Date prevMonthStart = evalDatePrevPeriodStart(date, EPeriod.MONTHLY);
+    Date prevMonthStart = evDatePrevPeriodStart(date, EPeriod.MONTHLY, this.cal);
     System.out.println("t1 - prev month start is " + prevMonthStart);
     assertEquals(new Date(1467320400000L), prevMonthStart); //Thu Jul 01 2016 00:00:00
 
@@ -85,41 +86,113 @@ public class CalendarTest {
 
     System.out.println("t2 - date is " + date);
 
-    periodDayStart = evalDatePeriodStartFor(date, EPeriod.DAILY);
+    periodDayStart = evDatePeriodStart(date, EPeriod.DAILY, this.cal);
     System.out.println("t2 - period day start is " + periodDayStart);
     assertEquals(new Date(1451595600000L), periodDayStart); //Fri Jan 01 2016 00:00:00
-    periodWeekStart = evalDatePeriodStartFor(date, EPeriod.WEEKLY);
+    periodWeekStart = evDatePeriodStart(date, EPeriod.WEEKLY, this.cal);
     System.out.println("t2 - period week start is " + periodWeekStart);
     assertEquals(new Date(1451163600000L), periodWeekStart); //Sun Dec 27 2015 00:00:00 USA standard!!!
-    periodMonthStart = evalDatePeriodStartFor(date, EPeriod.MONTHLY);
+    periodMonthStart = evDatePeriodStart(date, EPeriod.MONTHLY, this.cal);
     System.out.println("t2 - period month start is " + periodMonthStart);
     assertEquals(new Date(1451595600000L), periodMonthStart); //Fri Jan 01 2016 00:00:00
 
     date = new Date(1483185720000L); // Sat Dec 31 2016 15:02:00
     System.out.println("t2 - date is " + date);
-    nextDayStart = evalDateNextPeriodStart(date, EPeriod.DAILY);
+    nextDayStart = evalDateNextPeriodStart(date, EPeriod.DAILY, this.cal);
     System.out.println("t2 - next day start is " + nextDayStart);
     assertEquals(new Date(1483218000000L), nextDayStart);
-    nextWeekStart = evalDateNextPeriodStart(date, EPeriod.WEEKLY);
+    nextWeekStart = evalDateNextPeriodStart(date, EPeriod.WEEKLY, this.cal);
     System.out.println("t2 - next week start is " + nextWeekStart);
     assertEquals(new Date(1483218000000L), nextWeekStart); //Sun Jan 01 2017 00:00:00 USA standard!!!
-    nextMonthStart = evalDateNextPeriodStart(date, EPeriod.MONTHLY);
+    nextMonthStart = evalDateNextPeriodStart(date, EPeriod.MONTHLY, this.cal);
     System.out.println("t2 - next month start is " + nextMonthStart);
     assertEquals(new Date(1483218000000L), nextMonthStart);
 
-    prevDayStart = evalDatePrevPeriodStart(date, EPeriod.DAILY);
+    prevDayStart = evDatePrevPeriodStart(date, EPeriod.DAILY, this.cal);
     System.out.println("t2 - prev day start is " + prevDayStart);
     assertEquals(new Date(1483045200000L), prevDayStart); //Fri Dec 30 2016 00:00:00
-    prevWeekStart = evalDatePrevPeriodStart(date, EPeriod.WEEKLY);
+    prevWeekStart = evDatePrevPeriodStart(date, EPeriod.WEEKLY, this.cal);
     System.out.println("t2 - prev week start is " + prevWeekStart);
     assertEquals(new Date(1482008400000L), prevWeekStart); //Sun Jan 18 2016 00:00:00 USA standard!!!
-    prevMonthStart = evalDatePrevPeriodStart(date, EPeriod.MONTHLY);
+    prevMonthStart = evDatePrevPeriodStart(date, EPeriod.MONTHLY, this.cal);
     System.out.println("t2 - prev month start is " + prevMonthStart);
     assertEquals(new Date(1477947600000L), prevMonthStart); //Nov 01 2016 00:00:00
+  }
 
+  @Test
+  public void test2() throws Exception {
+    Date date = new Date(1470225720000L); // 3 Aug 2016 15:02
+    System.out.println("t1 - date is " + date);
+
+    Date periodDayStart = evDatePeriodStart(date, EPeriod.DAILY, this.calRu);
+    System.out.println("t1 - period day start is " + periodDayStart);
+    assertEquals(new Date(1470171600000L), periodDayStart); //Wed Aug 03 2016 00:00:00
+    Date periodWeekStart = evDatePeriodStart(date, EPeriod.WEEKLY, this.calRu);
+    System.out.println("t1 - period week start is " + periodWeekStart);
+    assertFalse(new Date(1469912400000L).equals(periodWeekStart)); //Sun Jul 31 2016 00:00:00 USA standard!!!
+    Date periodMonthStart = evDatePeriodStart(date, EPeriod.MONTHLY, this.calRu);
+    System.out.println("t1 - period month start is " + periodMonthStart);
+    assertEquals(new Date(1469998800000L), periodMonthStart); //Mon Aug 01 2016 00:00:00
+
+    Date nextDayStart = evalDateNextPeriodStart(date, EPeriod.DAILY, this.calRu);
+    System.out.println("t1 - next day start is " + nextDayStart);
+    assertEquals(new Date(1470258000000L), nextDayStart); //Thu Aug 04 2016 00:00:00
+    Date nextWeekStart = evalDateNextPeriodStart(date, EPeriod.WEEKLY, this.calRu);
+    System.out.println("t1 - next week start is " + nextWeekStart);
+    assertFalse(new Date(1470517200000L).equals(nextWeekStart)); //Sun Jul 07 2016 00:00:00 USA standard!!!
+    Date nextMonthStart = evalDateNextPeriodStart(date, EPeriod.MONTHLY, this.calRu);
+    System.out.println("t1 - next month start is " + nextMonthStart);
+    assertEquals(new Date(1472677200000L), nextMonthStart); //Thu Sep 01 2016 00:00:00
+
+    Date prevDayStart = evDatePrevPeriodStart(date, EPeriod.DAILY, this.calRu);
+    System.out.println("t1 - prev day start is " + prevDayStart);
+    assertEquals(new Date(1470085200000L), prevDayStart); //Thu Aug 02 2016 00:00:00
+    Date prevWeekStart = evDatePrevPeriodStart(date, EPeriod.WEEKLY, this.calRu);
+    System.out.println("t1 - prev week start is " + prevWeekStart);
+    assertFalse(new Date(1469307600000L).equals(prevWeekStart)); //Sun Jul 24 2016 00:00:00 USA standard!!!
+    Date prevMonthStart = evDatePrevPeriodStart(date, EPeriod.MONTHLY, this.calRu);
+    System.out.println("t1 - prev month start is " + prevMonthStart);
+    assertEquals(new Date(1467320400000L), prevMonthStart); //Thu Jul 01 2016 00:00:00
+
+    date = new Date(1451649720000L); //Fri Jan 01 2016 15:02:00
+
+    System.out.println("t2 - date is " + date);
+
+    periodDayStart = evDatePeriodStart(date, EPeriod.DAILY, this.calRu);
+    System.out.println("t2 - period day start is " + periodDayStart);
+    assertEquals(new Date(1451595600000L), periodDayStart); //Fri Jan 01 2016 00:00:00
+    periodWeekStart = evDatePeriodStart(date, EPeriod.WEEKLY, this.calRu);
+    System.out.println("t2 - period week start is " + periodWeekStart);
+    assertFalse(new Date(1451163600000L).equals(periodWeekStart)); //Sun Dec 27 2015 00:00:00 USA standard!!!
+    periodMonthStart = evDatePeriodStart(date, EPeriod.MONTHLY, this.calRu);
+    System.out.println("t2 - period month start is " + periodMonthStart);
+    assertEquals(new Date(1451595600000L), periodMonthStart); //Fri Jan 01 2016 00:00:00
+
+    date = new Date(1483185720000L); // Sat Dec 31 2016 15:02:00
+    System.out.println("t2 - date is " + date);
+    nextDayStart = evalDateNextPeriodStart(date, EPeriod.DAILY, this.calRu);
+    System.out.println("t2 - next day start is " + nextDayStart);
+    assertEquals(new Date(1483218000000L), nextDayStart);
+    nextWeekStart = evalDateNextPeriodStart(date, EPeriod.WEEKLY, this.calRu);
+    System.out.println("t2 - next week start is " + nextWeekStart);
+    assertFalse(new Date(1483218000000L).equals(nextWeekStart)); //Sun Jan 01 2017 00:00:00 USA standard!!!
+    nextMonthStart = evalDateNextPeriodStart(date, EPeriod.MONTHLY, this.calRu);
+    System.out.println("t2 - next month start is " + nextMonthStart);
+    assertEquals(new Date(1483218000000L), nextMonthStart);
+
+    prevDayStart = evDatePrevPeriodStart(date, EPeriod.DAILY, this.calRu);
+    System.out.println("t2 - prev day start is " + prevDayStart);
+    assertEquals(new Date(1483045200000L), prevDayStart); //Fri Dec 30 2016 00:00:00
+    prevWeekStart = evDatePrevPeriodStart(date, EPeriod.WEEKLY, this.calRu);
+    System.out.println("t2 - prev week start is " + prevWeekStart);
+    assertFalse(new Date(1482008400000L).equals(prevWeekStart)); //Sun Jan 18 2016 00:00:00 USA standard!!!
+    prevMonthStart = evDatePrevPeriodStart(date, EPeriod.MONTHLY, this.calRu);
+    System.out.println("t2 - prev month start is " + prevMonthStart);
+    assertEquals(new Date(1477947600000L), prevMonthStart); //Nov 01 2016 00:00:00
   }
   
   Calendar cal = Calendar.getInstance(new Locale("en", "US"));
+  Calendar calRu = Calendar.getInstance(new Locale("ru", "RU"));
 
   /**
    * <p>Evaluate date start of next balance store period.</p>
@@ -127,22 +200,22 @@ public class CalendarTest {
    * @return Start of next period nearest to pDateFor
    * @throws Exception - an exception
    **/
-  public final Date evalDateNextPeriodStart(Date pDateFor, EPeriod period) {
-    cal.setTime(pDateFor);
-    cal.set(Calendar.HOUR_OF_DAY, 0);
-    cal.set(Calendar.MINUTE, 0);
-    cal.set(Calendar.SECOND, 0);
-    cal.set(Calendar.MILLISECOND, 0);
+  public final Date evalDateNextPeriodStart(Date pDateFor, EPeriod period, Calendar pCal) {
+    pCal.setTime(pDateFor);
+    pCal.set(Calendar.HOUR_OF_DAY, 0);
+    pCal.set(Calendar.MINUTE, 0);
+    pCal.set(Calendar.SECOND, 0);
+    pCal.set(Calendar.MILLISECOND, 0);
     if (period.equals(EPeriod.DAILY)) {
-      cal.add(Calendar.DATE, 1);
+      pCal.add(Calendar.DATE, 1);
     } else if (period.equals(EPeriod.MONTHLY)) {
-      cal.add(Calendar.MONTH, 1);
-      cal.set(Calendar.DAY_OF_MONTH, 1);
+      pCal.add(Calendar.MONTH, 1);
+      pCal.set(Calendar.DAY_OF_MONTH, 1);
     } else if (period.equals(EPeriod.WEEKLY)) {
-      cal.add(Calendar.DAY_OF_YEAR, 7);
-      cal.set(Calendar.DAY_OF_WEEK, 1);
+      pCal.add(Calendar.DAY_OF_YEAR, 7);
+      pCal.set(Calendar.DAY_OF_WEEK, 1);
     }
-    return cal.getTime();
+    return pCal.getTime();
   }
 
   /**
@@ -151,22 +224,22 @@ public class CalendarTest {
    * @return Start of next period nearest to pDateFor
    * @throws Exception - an exception
    **/
-  public final Date evalDatePrevPeriodStart(Date pDateFor, EPeriod period) {
-    cal.setTime(pDateFor);
-    cal.set(Calendar.HOUR_OF_DAY, 0);
-    cal.set(Calendar.MINUTE, 0);
-    cal.set(Calendar.SECOND, 0);
-    cal.set(Calendar.MILLISECOND, 0);
+  public final Date evDatePrevPeriodStart(Date pDateFor, EPeriod period, Calendar pCal) {
+    pCal.setTime(pDateFor);
+    pCal.set(Calendar.HOUR_OF_DAY, 0);
+    pCal.set(Calendar.MINUTE, 0);
+    pCal.set(Calendar.SECOND, 0);
+    pCal.set(Calendar.MILLISECOND, 0);
     if (period.equals(EPeriod.DAILY)) {
-      cal.add(Calendar.DATE, -1);
+      pCal.add(Calendar.DATE, -1);
     } else if (period.equals(EPeriod.MONTHLY)) {
-      cal.add(Calendar.MONTH, -1);
-      cal.set(Calendar.DAY_OF_MONTH, 1);
+      pCal.add(Calendar.MONTH, -1);
+      pCal.set(Calendar.DAY_OF_MONTH, 1);
     } else if (period.equals(EPeriod.WEEKLY)) {
-      cal.add(Calendar.DAY_OF_YEAR, -7);
-      cal.set(Calendar.DAY_OF_WEEK, 1);
+      pCal.add(Calendar.DAY_OF_YEAR, -7);
+      pCal.set(Calendar.DAY_OF_WEEK, 1);
     }
-    return cal.getTime();
+    return pCal.getTime();
   }
 
   /**
@@ -175,18 +248,18 @@ public class CalendarTest {
    * @return Start of period nearest to pDateFor
    * @throws Exception - an exception
    **/
-  public final synchronized Date evalDatePeriodStartFor(
-    Date pDateFor, EPeriod period) {
-    cal.setTime(pDateFor);
-    cal.set(Calendar.HOUR_OF_DAY, 0);
-    cal.set(Calendar.MINUTE, 0);
-    cal.set(Calendar.SECOND, 0);
-    cal.set(Calendar.MILLISECOND, 0); //Daily is ready
+  public final synchronized Date evDatePeriodStart(
+    Date pDateFor, EPeriod period, Calendar pCal) {
+    pCal.setTime(pDateFor);
+    pCal.set(Calendar.HOUR_OF_DAY, 0);
+    pCal.set(Calendar.MINUTE, 0);
+    pCal.set(Calendar.SECOND, 0);
+    pCal.set(Calendar.MILLISECOND, 0); //Daily is ready
     if (period.equals(EPeriod.MONTHLY)) {
-      cal.set(Calendar.DAY_OF_MONTH, 1);
+      pCal.set(Calendar.DAY_OF_MONTH, 1);
     } else if (period.equals(EPeriod.WEEKLY)) {
-      cal.set(Calendar.DAY_OF_WEEK, 1);
+      pCal.set(Calendar.DAY_OF_WEEK, 1);
     }
-    return cal.getTime();
+    return pCal.getTime();
   }
 }
